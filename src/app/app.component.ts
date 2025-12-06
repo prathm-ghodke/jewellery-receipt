dd3e803b2ca961a5b233bdbf378f64ac1bdd699d
PrAtHm3545 <prathmeshghodke3545@gmail.com>
2025-12-06 18:44:21 +0530

    Deleted receipt component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import { GoldDetailComponent } from '../gold-detail/gold-detail.component';
 import { CustomerDetailComponent } from "../customer-detail/customer-detail.component";
-import { ReceiptComponent } from "../receipt/receipt.component";
 @Component({
   selector: 'app-root',
-  imports: [MatTabsModule, GoldDetailComponent, CustomerDetailComponent, ReceiptComponent],
+  imports: [MatTabsModule, GoldDetailComponent, CustomerDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
